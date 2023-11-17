fb88d4caa591714cedcf5ea09b1efcd5c53c6f08
Renata <opel5323@gmail.com>
2023-11-17 23:42:36 +0300

    137.kt

---
+++ 137.kt
@@ -0,0 +1,20 @@
+fun main(){
+    // числа
+    var q: Int = 12 ;println(q)
+    val w: Byte = 11 ;println(w)
+    val e:Short=13; println(e)
+    var r:Long =10; println(r)
+    var t:UInt=14U;println(t)
+    val y: UByte =9U; println(y)
+    val u:UShort=15U;println(u)
+    var i: ULong=8U;println(i)
+    // Плавающая точка
+    var o: Float=1.6F;println(o)
+    val p: Double=0.7;println(p)
+    //Символы
+    val l: Char = 'q';println(l)
+    //Строки
+    var k:String ="qwe";println(k)
+	//Логика
+	var j : Boolean= true; println(j)
+}
